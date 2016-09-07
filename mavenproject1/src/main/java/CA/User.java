d13390dab97bcf1cbf77da2f3cc542f823c01492
Philip <Philip@10.50.138.35>
2016-09-07 10:02:50 +0200

    Idk

---
+++ mavenproject1/src/main/java/CA/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CA;
+
+import java.net.Socket;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static CA.TCPServer.ss;
+
+/**
+ *
+ * @author Mathias
+ */
+public class User extends Thread
+{
+
+  String username;
+
+  public User(String username)
+  {
+    this.username = username;
+  }
+
+
+  public void run()
+  {
+    
+  }
+}
